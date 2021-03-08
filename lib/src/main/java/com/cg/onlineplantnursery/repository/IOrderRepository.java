c5a11d0c002fc1236cb55c068d81dfa89e659625
kamalesh-20 <kamalsagiraju@gmail.com>
2021-03-08 12:09:56 +0530

    message

--- lib/src/main/java/com/cg/onlineplantnursery/repository/IOrderRepository.java
+++ lib/src/main/java/com/cg/onlineplantnursery/repository/IOrderRepository.java
@@ -5,10 +5,16 @@ import java.util.List;
 import com.cg.onlineplantnursery.entity.Order;
 
 public interface IOrderRepository {
+	
+	//Is my comment added
 	Order addOrder(Order order);
+	
 	Order updateOrder(Order order);
+	
 	Order deleteOrder(int orderId);
+	
 	Order viewOrder(int  orderId);
+	
 	List<Order> viewAllOrders();
-
+	
 }
--- lib/src/main/java/com/cg/onlineplantnursery/service/IOrderService.java
+++ lib/src/main/java/com/cg/onlineplantnursery/service/IOrderService.java
@@ -6,8 +6,12 @@ import com.cg.onlineplantnursery.entity.Order;
 
 public interface IOrderService {
 	Order addOrder(Order order);
+	
 	Order updateOrder(Order order);
+	
 	Order deleteOrder(int orderId);
+	
 	Order viewOrder(int  orderId);
+	
 	List<Order> viewAllOrders();
 }
